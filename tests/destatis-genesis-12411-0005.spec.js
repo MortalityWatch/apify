7c16200cb7b686743d9b5412201f4d7b36933dd0
MortalityWatch <mortalitywatch@protonmail.com>
2024-07-28 17:17:01 +0200

    add all years for 12411-0005

--- tests/destatis-genesis-12411-0005.spec.js
+++ tests/destatis-genesis-12411-0005.spec.js
@@ -5,6 +5,9 @@ test('test', async ({ page }) => {
   await page.goto(
     'https://www-genesis.destatis.de/genesis/online?operation=table&code=12411-0005#astructure'
   )
+  await page.getByRole('button', { name: 'Zeit auswählen' }).click()
+  await page.getByText('Alle verfügbaren Zeitangaben').click()
+  await page.getByRole('button', { name: 'übernehmen' }).click()
 
   await dl(page, 'destatis-genesis-12411-0005')
 })
