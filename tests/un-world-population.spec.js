16006745c7c46eb6be63d0883b4005a5cfba0213
MortalityWatch <mortalitywatch@protonmail.com>
2024-12-19 09:29:17 -0500

    fix un world pop

--- tests/un-world-population.spec.js
+++ tests/un-world-population.spec.js
@@ -1,10 +1,12 @@
 import { test } from '@playwright/test'
 
 test('test', async ({ page }) => {
-  await page.goto('https://population.un.org/wpp/Download/Standard/MostUsed/')
+  await page.goto('https://population.un.org/wpp/downloads')
   const downloadPromise = page.waitForEvent('download')
   await page
-    .getByRole('link', { name: 'Compact (most used: estimates' })
+    .getByRole('link', {
+      name: 'Compact (most used: estimates and medium projections) (XLSX)',
+    })
     .click()
   const download = await downloadPromise
   await download.saveAs(`./temp/un-world-population.xlsx`)
